a5b2a0b6c9e4bf46a124b03d3ac5f15ce9000fcb
Hanbi <rlagksql219@naver.com>
2023-01-12 18:38:28 +0900

    1459 finish

--- BOJ/Greedy/1459 걷기.cpp
+++ BOJ/Greedy/1459 걷기.cpp
@@ -5,11 +5,13 @@ using namespace std;
 
 int main() {
 	int x, y, w, s;
-	int ans = 0;
+	long long ans = 0;
 	int cx = 0, cy = 0;
 
 	cin >> x >> y >> w >> s;
-	while (cx >= x && cy >= y) {
+	while (1) {
+		if (cx >= x || cy >= y)	break;
+
 		if (2 * w < s) { //가로,세로 이동
 			cx += 1;
 			cy += 1;
@@ -22,18 +24,27 @@ int main() {
 		}
 	}
 
- 	while (cx != x) {
+	while (cx != x) {
+		if (((x - cx) % 2 == 0) && (2*w > 2*s)) {
+			cx += 2;
+			ans += 2 * s;
+			continue;
+		}
 		cx += 1;
 		ans += w;
 	}
 
 	while (cy != y) {
+		if (((y - cy) % 2 == 0) && (2*w > 2*s)) {
+			cy += 2;
+			ans += 2 * s;
+			continue;
+		}
 		cy += 1;
 		ans += w;
 	}
 
 	cout << ans;
-//
 
 	return 0;
 }
