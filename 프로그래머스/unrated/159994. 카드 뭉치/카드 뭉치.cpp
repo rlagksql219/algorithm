dd99d96d10f47c2196de9bf1a14d9556b60a9577
Hanbi <69866091+rlagksql219@users.noreply.github.com>
2023-03-08 16:12:32 +0900

    Update 카드 뭉치.cpp

--- 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.cpp
+++ 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.cpp
@@ -8,13 +8,6 @@ string solution(vector<string> cards1, vector<string> cards2, vector<string> goa
     vector<int> mark;
     
     for(int i=0; i<goal.size(); i++) {
-        /*for(int j=0; j<cards1.size(); j++) {
-            if(goal[i] == cards1[j]) {
-                cards1.erase(cards1.begin());
-                mark.push_back(1);
-                break;
-            }
-        }*/
         if(goal[i] == cards1[0]) {
             cards1.erase(cards1.begin());
             mark.push_back(1);
@@ -26,20 +19,13 @@ string solution(vector<string> cards1, vector<string> cards2, vector<string> goa
             mark.push_back(1);
             continue;
         }
-        
-        /*for(int j=0; j<cards2.size(); j++) {
-            if(goal[i] == cards2[j]) {
-                cards2.erase(cards2.begin());
-                mark.push_back(1);
-                break;
-            }
-        }*/
     }
     
     if(mark.size() != goal.size())
         answer = "No";
     else
         answer = "Yes";
+    
     
     return answer;
 }
