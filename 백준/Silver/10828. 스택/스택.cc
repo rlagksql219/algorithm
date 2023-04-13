68645d8a0cad4d99267a03da0be7f3bfd9dbccb6
Hanbi <69866091+rlagksql219@users.noreply.github.com>
2023-04-13 17:13:57 +0900

    [Silver IV] Title: 스택, Time: 4 ms, Memory: 2156 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.cc
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <stack>
+#include <string>
+
+using namespace std;
+
+int main() {
+	int n;
+	int result = 0;
+	string cmd;
+	int num;
+	stack<int> s;
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> cmd;
+
+		if (cmd == "pop") {
+			if (s.size() == 0) {
+				result = -1;
+				cout << result << '\n';
+			}
+			else {
+				result = s.top();
+				cout << result << '\n';
+				s.pop();
+			}
+		}
+		else if (cmd == "size") {
+			cout << s.size() << '\n';
+		}
+		else if (cmd == "empty") {
+			if (s.size() == 0) {
+				result = 1;
+				cout << result << '\n';
+			}
+			else {
+				result = 0;
+				cout << result << '\n';
+			}
+		}
+		else if (cmd == "top") {
+			if (s.size() == 0) {
+				result = -1;
+				cout << result << '\n';
+			}
+			else {
+				result = s.top();
+				cout << result << '\n';
+			}
+		}
+		else if (cmd == "push") {
+			cin >> num;
+			s.push(num);
+		}
+	}
+
+	return 0;
+}
