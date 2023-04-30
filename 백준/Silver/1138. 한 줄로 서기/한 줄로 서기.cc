5e406ff52f376a62984f4ebb932009f5626be1c7
Hanbi <69866091+rlagksql219@users.noreply.github.com>
2023-05-01 03:15:27 +0900

    [Silver II] Title: 한 줄로 서기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.cc
+++ 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.cc
@@ -1,25 +1,28 @@
 #include <iostream>
+
 using namespace std;
 
-int a[11], ans[11];
+int arr[10];
 
 int main() {
-    int n;
-    cin >> n;
-    for (int i = 1; i <= n; i++) {
-        cin >> a[i];
-        int cnt = 0;
-        for (int j = 1; j <= n; j++) {
-            if (cnt == a[i] && ans[j] == 0) {
-                ans[j] = i;
-                break;
-            }
-            if (ans[j] == 0) cnt++;
-        }
-    }
-    for (int i = 1; i <= n; i++) {
-        cout << ans[i] << ' ';
-    }
-    cout << '\n';
-    return 0;
+
+	int n;
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		int cnt = 0;
+		cin >> cnt;
+
+		for (int j = 0; j < n; j++) {
+			if (cnt == 0 && arr[j] == 0) {
+				arr[j] = i;
+				break;
+			}
+			if (arr[j] == 0)	cnt--;
+		}
+	}
+
+	for (int i = 0; i < n; i++)
+		cout << arr[i] << " ";
+
+	return 0;
 }
