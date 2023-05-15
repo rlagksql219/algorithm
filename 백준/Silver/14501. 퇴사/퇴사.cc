2df09968dd8bd5e3a891acf342bb51035234b1a5
Hanbi <69866091+rlagksql219@users.noreply.github.com>
2023-05-15 18:07:44 +0900

    [Silver III] Title: 퇴사, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/14501. 퇴사/퇴사.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int t[16];
+int p[16];
+int dp[16];
+
+int main() {
+	int N;
+	
+	cin >> N;
+	for (int i = 1; i <= N; i++)
+		cin >> t[i] >> p[i];
+
+	for (int i = N; i > 0; i--) {
+		if ((i + t[i] - 1) > N)
+			dp[i] = dp[i + 1];
+		else
+			dp[i] = max(dp[i + 1], p[i] + dp[i + t[i]]);
+	}
+
+	cout << dp[1] << '\n';
+
+	return 0;
+}
