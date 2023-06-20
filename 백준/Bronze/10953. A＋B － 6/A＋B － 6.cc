51e21566a3e8e823c3cf12693c0cf78ebe9c58dd
Hanbi <69866091+rlagksql219@users.noreply.github.com>
2023-06-21 00:22:59 +0900

    [Bronze III] Title: A+B - 6, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10953. A＋B － 6/A＋B － 6.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int T;
+
+	cin >> T;
+	while (T--) {
+		int A, B;
+		char c;
+
+		cin >> A >> c >> B;
+		cout << A + B << '\n';
+	}
+
+	return 0;
+}
