110155a4220ccde3ff18e1db4d5ee91a53114f6d
Hanbi <69866091+rlagksql219@users.noreply.github.com>
2023-06-21 01:57:31 +0900

    [Bronze III] Title: 별 찍기 - 16, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10991. 별 찍기 － 16/별 찍기 － 16.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int n;
+
+	cin >> n;
+	int length = 2 * n - 1;
+	for (int i = 1; i <= n; i++) {
+		for (int j = n - i; j > 0; j--) {
+			cout << " ";
+		}
+
+		int num = 2 * i - 1;
+		for (int j = 0; j < num; j++) {
+			if (j % 2 == 0) {
+				cout << "*";
+			}
+			else {
+				cout << " ";
+			}
+		}
+		cout << '\n';
+	}
+
+	return 0;
+}
