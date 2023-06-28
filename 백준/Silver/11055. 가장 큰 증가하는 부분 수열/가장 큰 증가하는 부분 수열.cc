b593ed2c6d290fdb2f004c42209c0803134ac2a7
Hanbi <69866091+rlagksql219@users.noreply.github.com>
2023-06-28 18:45:37 +0900

    [Silver II] Title: 가장 큰 증가하는 부분 수열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int dp[1001];
+
+int main() {
+	int N;
+	vector<int> v;
+	int ans = 0;
+
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		int tmp;
+
+		cin >> tmp;
+		v.push_back(tmp);
+	}
+
+	for (int i = 0; i < N; i++) {
+		dp[i] = v[i];
+		for (int j = 0; j < i; j++) {
+			if (v[i] > v[j]) {
+				if (dp[i] < dp[j] + v[i])
+					dp[i] = dp[j] + v[i];
+			}
+		}
+		ans = max(ans, dp[i]);
+	}
+
+	cout << ans;
+
+	return 0;
+}
