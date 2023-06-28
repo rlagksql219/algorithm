5755aa130d08d70a4889f07b4e76eb02320d1725
Hanbi <69866091+rlagksql219@users.noreply.github.com>
2023-06-28 17:15:09 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int dp[1001];
+
+int main() {
+	int N;
+	vector<int> v;
+	int ans = 0;
+
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		int tmp;
+
+		cin >> tmp;
+		v.push_back(tmp);
+	}
+
+	for (int i = 0; i < N; i++) {
+		dp[i] = 1;
+
+		for (int j = 0; j < i; j++) {
+			if (v[i] > v[j])
+				dp[i] = max(dp[i], dp[j] + 1);
+		}
+		ans = max(ans, dp[i]);
+	}
+
+	cout << ans;
+
+	return 0;
+}
