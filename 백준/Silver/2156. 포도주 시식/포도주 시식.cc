88bcdc6167f5a3158288a4d870b3b51f10039220
Hanbi <69866091+rlagksql219@users.noreply.github.com>
2023-07-01 17:21:02 +0900

    Update 포도주 시식.cc

--- 백준/Silver/2156. 포도주 시식/포도주 시식.cc
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.cc
@@ -19,8 +19,8 @@ int main() {
 	dp[0] = v[0];
 	dp[1] = dp[0] + v[1];
 	for (int i = 2; i < n; i++) {
-		dp[i] = max(dp[i - 3] + v[i - 1] + v[i], dp[i - 2] + v[i]);
-		dp[i] = max(dp[i - 1], dp[i]);
+		dp[i] = max(dp[i - 3] + v[i - 1] + v[i], dp[i - 2] + v[i]); //i번째 잔을 마셨을 때, (i-1)번째 잔을 마신 경우와 안 마신 경우 중에 최댓값
+		dp[i] = max(dp[i - 1], dp[i]); //i번째 잔을 마신 경우와 안 마신 경우 중에 최댓값
 	}
 
 	cout << dp[n-1];
