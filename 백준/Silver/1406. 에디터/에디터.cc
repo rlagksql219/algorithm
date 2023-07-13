0c876a1875adf6a4e18b90bb3be2b3c753c26659
Hanbi <69866091+rlagksql219@users.noreply.github.com>
2023-07-13 16:28:03 +0900

    Update 에디터.cc

--- 백준/Silver/1406. 에디터/에디터.cc
+++ 백준/Silver/1406. 에디터/에디터.cc
@@ -1,5 +1,6 @@
-/*#include <string>
+#include <string>
 #include <iostream>
+#include <list>
 
 using namespace std;
 
@@ -8,107 +9,34 @@ int main() {
 	int n;
 
 	cin >> str >> n;
-	int cursor = str.size();
+	list<char> l(str.begin(), str.end());
+	auto cursor = l.end();
 	for (int i = 0; i < n; i++) {
 		char tmp;
 		cin >> tmp;
 
 		if (tmp == 'L') {
-			if (cursor != 0)
+			if (cursor != l.begin())
 				cursor--;
 		}
 		else if (tmp == 'D') {
-			if (cursor != str.size())
+			if (cursor != l.end())
 				cursor++;
 		}
 		else if (tmp == 'B') {
-			if (cursor != 0) {
-				str.erase(cursor - 1);
-				//str = str.substr(0, str.size() - 1);
-				cursor--;
+			if (cursor != l.begin()) {
+				cursor = l.erase(--cursor);
 			}
 		}
 		else {
-			string insert_tmp;
+			char insert_tmp;
 			cin >> insert_tmp;
-			str.insert(cursor, insert_tmp);
-			cursor++;
+			l.insert(cursor, insert_tmp);
 		}
 	}
 
-	cout << str;
+	for (auto iter = l.begin(); iter != l.end(); iter++)
+		cout << *iter;
 
 	return 0;
-}*/
-
-
-#include <iostream>
-#include <string>
-#include <stack>
-using namespace std;
-
-int main()
-{
-    int M;
-    string s = "";
-    stack<char> left;
-    stack<char> right;
-
-    cin >> s;
-
-    for (int i = 0; i < (int)s.size(); i++)
-    {
-        left.push(s[i]);
-    }
-
-    cin >> M;
-
-    for (int i = 0; i < M; i++)
-    {
-        char cmd, c;
-        cin >> cmd;
-
-        if (cmd == 'L')
-        {
-            if (!left.empty())
-            {
-                right.push(left.top());
-                left.pop();
-            }
-        }
-        else if (cmd == 'D')
-        {
-            if (!right.empty())
-            {
-                left.push(right.top());
-                right.pop();
-            }
-        }
-        else if (cmd == 'B')
-        {
-            if (!left.empty())
-            {
-                left.pop();
-            }
-        }
-        else if (cmd == 'P')
-        {
-            cin >> c;
-            left.push(c);
-        }
-    }
-
-    // left에 있는 원소들 모두 right로 이동!
-    while (!left.empty())
-    {
-        right.push(left.top());
-        left.pop();
-    }
-
-    // right에 있는 원소 출력
-    while (!right.empty())
-    {
-        cout << right.top();
-        right.pop();
-    }
 }
