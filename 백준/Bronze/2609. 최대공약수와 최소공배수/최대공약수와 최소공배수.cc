bebf3cfe7fb82c7b4e05575b60895ad9afddf0e6
Hanbi <69866091+rlagksql219@users.noreply.github.com>
2023-07-17 19:25:34 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+<유클리드 호제법>
+정수 a, b (a>=b)
+r = a % b
+
+a와 b의 최대공약수 (a, b)
+=> (a,b) = (b, r)
+
+
+a와 b의 최소공배수
+=> a * b / (a, b)
+*/
+
+
+int gcd(int a, int b) {
+	if (b == 0)	
+		return a;
+	else
+		return gcd(b, a % b);
+}
+
+int main() {
+	int n1, n2;
+	int ans1 = 0; //최대공약수
+	int ans2 = 0; //최소공배수
+
+	cin >> n1 >> n2;
+	if (n1 > n2)
+		ans1 = gcd(n1, n2);
+	else
+		ans1 = gcd(n2, n1);
+
+	ans2 = n1 * n2 / ans1;
+	
+	cout << ans1 << '\n';
+	cout << ans2;
+
+	return 0;
+}
