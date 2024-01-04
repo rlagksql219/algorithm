29a89272d9a334c0d65218c0cac84c7c849f723d
Hanbi <69866091+rlagksql219@users.noreply.github.com>
2024-01-04 19:10:44 +0900

    [level unrated] Title: 피자 나눠 먹기 (2), Time: 0.01 ms, Memory: 4.18 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    
+    for(int i=1; i<=100; i++) {
+        if((n * i) % 6 == 0) {
+            answer = (n * i) / 6;
+            break;
+        }
+    }
+    
+    return answer;
+}
