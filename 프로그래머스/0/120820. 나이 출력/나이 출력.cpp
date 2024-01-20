8299911050c3f924c2aec5ae01be4e323c3c2e80
Hanbi <69866091+rlagksql219@users.noreply.github.com>
2024-01-20 23:40:21 +0900

    [level 0] Title: 나이 출력, Time: 0.01 ms, Memory: 3.69 MB -BaekjoonHub

---
+++ 프로그래머스/0/120820. 나이 출력/나이 출력.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int age) {
+    int answer = 0;
+    
+    answer += 2022;
+    answer -= age;
+    answer++;
+    
+    return answer;
+}
