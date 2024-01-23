8fcaa328fc544a1c7e6f70d6b5d443059b56239b
Hanbi <69866091+rlagksql219@users.noreply.github.com>
2024-01-23 16:36:24 +0900

    [Silver IV] Title: 스위치 켜고 끄기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.cc
+++ 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.cc
@@ -3,51 +3,49 @@
 
 using namespace std;
 
+int arr[101];
+
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 
-    int N, M;
-    cin >> N;
+	int N, M;
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		int tmp;
+		cin >> tmp;
+		arr[i] = tmp;
+	}
 
-    vector<int> arr(N);
-    for (int i = 0; i < N; i++) {
-        int tmp;
-        cin >> tmp;
-        arr[i] = tmp;
-    }
+	cin >> M;
+	for (int i = 0; i < M; i++) {
+		int what, num;
+		cin >> what >> num;
 
-    cin >> M;
-    for (int i = 0; i < M; i++) {
-        int what, num;
-        cin >> what >> num;
+		if (what == 1) { //남
+			for (int j = num - 1; j < N; j += num) {
+				arr[j] = !arr[j];
+			}
+		}
+		else { //여
+			int left = num - 2;
+			int right = num;
 
-        if (what == 1) { // 남
-            for (int j = num - 1; j < N; j += num) {
-                arr[j] = !arr[j];
-            }
-        } else { // 여
-            int left = num - 2;
-            int right = num;
-            while (left >= 0 && right < N && arr[left] == arr[right]) {
-                arr[left] = !arr[left];
-                arr[right] = !arr[right];
-                left--;
-                right++;
-            }
-            arr[num - 1] = !arr[num - 1];
-        }
-    }
+			arr[num - 1] = !arr[num - 1];
+			while (left >= 0 && right < N && arr[left] == arr[right]) {
+				arr[left] = !arr[left];
+				arr[right] = !arr[right];
+				left--;
+				right++;
+			}
+		}
+	}
 
-    for (int i = 0; i < N; i++) {
-        cout << arr[i] << " ";
-        
-        // 20개를 출력한 후 개행 추가
-        if ((i + 1) % 20 == 0) {
-            cout << "\n";
-        }
-    }
+	for (int i = 0; i < N; i++) {
+		cout << arr[i] << " ";
+		if ((i + 1) % 20 == 0)	cout << '\n';
+	}
 
-    return 0;
+	return 0;
 }
