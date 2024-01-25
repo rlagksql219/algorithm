e78f8fc862f2e6b720c699823d7e1cad8addd8c2
Hanbi <69866091+rlagksql219@users.noreply.github.com>
2024-01-25 18:14:53 +0900

    [Silver III] Title: 주유소, Time: 12 ms, Memory: 4372 KB, Score: 58 point -BaekjoonHub

---
+++ 백준/Silver/13305. 주유소/주유소.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	
+	int N;
+	cin >> N;
+	vector<long long> dist(N-1);
+	vector<long long> price(N);
+	vector<long long> min_price(N);
+	int ans = 0;
+
+	for (int i = 0; i < N - 1; i++) {
+		cin >> dist[i];
+	}
+	for (int i = 0; i < N; i++) {
+		cin >> price[i];
+	}
+
+	min_price[0] = price[0];
+	for (int i = 1; i < N; i++) {
+		min_price[i] = min(min_price[i - 1], price[i]);
+	}
+
+	for (int i = 0; i < N - 1; i++) {
+		ans += dist[i] * min_price[i];
+	}
+
+	cout << ans;
+
+	return 0;
+}
