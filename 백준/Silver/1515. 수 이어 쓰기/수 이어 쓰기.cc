d21bdf76241721dae753bae08cc09a27d7fb99a1
Hanbi <69866091+rlagksql219@users.noreply.github.com>
2024-01-31 18:51:31 +0900

    [Silver III] Title: 수 이어 쓰기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/1515. 수 이어 쓰기/수 이어 쓰기.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int num = 0;
+	int ptr = 0;
+	string str;
+	cin >> str;
+	while (num++ < 30000) {
+		string s = to_string(num);
+		for (int i = 0; i < s.length(); i++) {
+			if (str[ptr] == s[i])	ptr++;
+			if (ptr == str.length()) {
+				cout << num;
+				return 0;
+			}
+		}
+	}
+
+	return 0;
+}
