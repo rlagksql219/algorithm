be65a3de3f94fad0c95d7623c537a885d99902ac
Hanbi <69866091+rlagksql219@users.noreply.github.com>
2024-02-13 17:02:56 +0900

    [Silver II] Title: KCPC, Time: 12 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/3758. KCPC/KCPC.cc
+++ 백준/Silver/3758. KCPC/KCPC.cc
@@ -1,39 +1,46 @@
-#include <stdio.h>
-#include <cstring>
+#include <iostream>
 #include <vector>
 #include <algorithm>
+
 using namespace std;
- 
-int tc, n, k, t, m, a, b, c;
-int d[101][101], tim[101], cnt[101];
- 
+
 int main() {
-    scanf("%d", &tc);
-    while (tc--) {
-        memset(d, 0, sizeof(d));
-        memset(tim, 0, sizeof(tim));
-        memset(cnt, 0, sizeof(cnt));
-        scanf("%d%d%d%d", &n, &k, &t, &m);
-        t--;
-        for (int i = 0; i < m; i++) {
-            scanf("%d%d%d", &a, &b, &c);
-            a--; b--;
-            d[a][b] = max(d[a][b], c);
-            cnt[a]++;
-            tim[a] = i;
-        }
-        vector < pair<int, pair<int, pair<int, int>>>> v;
-        for (int i = 0; i < n; i++) {
-            int sum = 0;
-            for (int j = 0; j < k; j++) 
-                sum += d[i][j];
-            v.push_back({ sum,{-cnt[i],{-tim[i],i}} });
-        }
-        sort(v.begin(), v.end());
-        for (int i = 0; i < n; i++) {
-            if (v[i].second.second.second == t) printf("%d\n", n - i);
-        }
-    }
- 
-    return 0;
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int T;
+	cin >> T;
+	while (T--) {
+		int n, k, t, m;
+		cin >> n >> k >> t >> m;
+		int arr[101][101] = { 0 };
+		int cnt[101] = { 0 };
+		int order[101] = { 0 };
+		t--;
+		for (int i = 0; i < m; i++) {
+			int id, num, s;
+			cin >> id >> num >> s;
+			id--;
+			num--;
+			arr[id][num] = max(arr[id][num], s);
+			cnt[id]++;
+			order[id] = i;
+		}
+
+
+		vector < pair<int, pair<int, pair<int, int>>>> v; //총점, 제출횟수, 최종제출시간, 팀번호
+		for (int i = 0; i < n; i++) {
+			int sum = 0;
+			for (int j = 0; j < k; j++)
+				sum += arr[i][j];
+			v.push_back({ sum,{-cnt[i],{-order[i],i}} });
+		}
+		sort(v.begin(), v.end());
+		for (int i = 0; i < n; i++) {
+			if (v[i].second.second.second == t) printf("%d\n", n - i);
+		}
+	}
+
+	return 0;
 }
