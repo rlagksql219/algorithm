1f66ce0989dadfc698f0b5f57962ccb54fe2935c
Hanbi <69866091+rlagksql219@users.noreply.github.com>
2024-02-13 17:26:10 +0900

    [Silver III] Title: 타노스, Time: 0 ms, Memory: 2024 KB, Score: 25 point -BaekjoonHub

---
+++ 백준/Silver/20310. 타노스/타노스.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	string s;
+	int zero_cnt = 0;
+	int one_cnt = 0;
+	cin >> s;
+	for (int i = 0; i < s.length(); i++) {
+		if (s[i] == '0')	zero_cnt++;
+	}
+	one_cnt = s.length() - zero_cnt;
+	zero_cnt /= 2;
+	one_cnt /= 2;
+
+	for (int i = 0; i < s.length(); i++) {
+		if (one_cnt == 0)	break;
+		if (s[i] == '1') {
+			s.erase(s.begin() + i);
+			one_cnt--;
+		}
+	}
+	for (int i = s.length()-1; i >= 0; i--) {
+		if (zero_cnt == 0)	break;
+		if (s[i] == '0') {
+			s.erase(s.begin() + i);
+			zero_cnt--;
+		}
+	}
+
+	cout << s;
+
+	return 0;
+}
