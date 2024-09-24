b9a8509bfa47be1bc6dd69f86ec7baebcc33c023
Hanbi <rlagksql219@naver.com>
2024-09-24 18:52:45 +0900

    [Gold I] Title: 멀티탭 스케줄링, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/1700. 멀티탭 스케줄링/멀티탭 스케줄링.cc
+++ 백준/Gold/1700. 멀티탭 스케줄링/멀티탭 스케줄링.cc
@@ -1,57 +1,67 @@
 #include <iostream>
 #include <vector>
 #include <set>
+
 using namespace std;
 
 int main() {
-    int N, K;
-    cin >> N >> K;
-    vector<int> schedule(K);
-    for (int i = 0; i < K; i++) {
-        cin >> schedule[i];
-    }
-    
-    set<int> multitap;  // 현재 멀티탭에 꽂혀있는 기기들
-    int unplug_count = 0;
-    
-    for (int i = 0; i < K; i++) {
-        int device = schedule[i];
-        
-        // 이미 꽂혀 있는 경우
-        if (multitap.find(device) != multitap.end()) {
-            continue;
-        }
-        
-        // 멀티탭에 자리가 있는 경우
-        if (multitap.size() < N) {
-            multitap.insert(device);
-            continue;
-        }
-        
-        // 자리가 없으니 플러그를 빼야 함
-        int to_unplug = -1;
-        int farthest = -1;
-        
-        for (int plugged : multitap) {
-            int last_use = K;
-            for (int j = i + 1; j < K; j++) {
-                if (schedule[j] == plugged) {
-                    last_use = j;
-                    break;
-                }
-            }
-            if (last_use > farthest) {
-                farthest = last_use;
-                to_unplug = plugged;
-            }
-        }
-        
-        // 멀티탭에서 가장 나중에 사용될 기기를 빼고 새로운 기기를 꽂음
-        multitap.erase(to_unplug);
-        multitap.insert(device);
-        unplug_count++;
-    }
-    
-    cout << unplug_count << endl;
-    return 0;
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	int N, K;
+	cin >> N >> K;
+	vector<int> order(K);
+	for (int i = 0; i < K; i++) {
+		cin >> order[i];
+	}
+
+	set<int> multitap;
+	int ans = 0;
+
+	for (int i = 0; i < K; i++) {
+		int device = order[i];
+
+		if (multitap.find(device) != multitap.end())	continue; //이미 꽂혀 있는 경우
+
+		if (multitap.size() < N) { //멀티탭에 자리가 있는 경우
+			multitap.insert(device);
+			continue;
+		}
+
+
+		//자리가 없는 경우, 뭐를 뺄지 정해야 함
+		/*
+		1. 앞으로 사용하지 않는 기기
+		2. 젤 마지막에 사용하는 기기
+
+		가장 나중에 사용하는 기기를 찾기
+		초기값을 K로 초기화 -> 앞으로 사용하지 않는다면 젤 큰 값이라 우선적으로 뽑아야되는 기기로 선정됨
+		*/
+		int unplug = -1;
+		int last_time = -1;
+
+		for (int plug : multitap) {
+			int last_use = K;
+			for (int j = i + 1; j < K; j++) {
+				if (order[j] == plug) {
+					last_use = j;
+					break;
+				}
+			}
+
+			if (last_use > last_time) {
+				last_time = last_use;
+				unplug = plug;
+			}
+		}
+
+		multitap.erase(unplug);
+		multitap.insert(device);
+		ans++;
+	}
+
+	cout << ans;
+
+	return 0;
 }
